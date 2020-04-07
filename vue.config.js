1fb7003a618d3bfdd4ba73c4ce075070e4c8371d
Agate Blue <me@agate.blue>
2020-04-07 21:11:00 +0200

    Cache busting

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,13 @@ module.exports = {
       args[0].title = 'Tempo - Find your own rythm'
       return args
     })
-  }
+  },
+  pwa: {
+    name: 'Tempo',
+    themeColor: '#422D62',
+    msTileColor: '#422D62',
+    workboxOptions: {
+      cacheId: process.env['VUE_APP_CACHE_ID'] || 'v0',
+    },
+  },
 }
