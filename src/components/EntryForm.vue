6a5de213ff0699ea99fdcebb4aa7ec379b07e943
Agate <me@agate.blue>
2020-04-15 11:36:19 +0200

    Fixed a few UX issues with date / text not resetting

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -5,13 +5,12 @@
       :id="name"
       rows="4"
       ref="textarea"
-      autofocus
       @keydown.ctrl.enter.exact="submit"
       placeholder="What's going on in your life?"
       v-model="text"></textarea>
     <div class="inline field">
       <label for="date" class="inline">Date</label>
-      <flat-pickr class="inline" name="date" v-model="date" :config="{enableTime: true, position: 'below', time_24hr: true}"></flat-pickr>
+      <flat-pickr @on-open="setDefaultDate" placeholder="Now" class="inline" name="date" v-model="date" :config="{enableTime: true, position: 'below', time_24hr: true}"></flat-pickr>
     </div>
     <input class="right floated" type="submit" value="Send">
   </form>
@@ -35,14 +34,22 @@ export default {
     let date = new Date()
     if (this.entry) {
       text = this.entry.text
-      date = new Date(this.entry.date) || new Date()
+      date = new Date(this.entry.date) || null
     }
     return {
       text,
       date,
     }
   },
+  mounted () {
+    this.$refs.textarea.focus()
+  },
   methods: {
+    setDefaultDate () {
+      if (!this.date) {
+        this.date = new Date()
+      }
+    },
     async submit () {
       if (!this.text) {
         return
@@ -52,17 +59,19 @@ export default {
       } else {
         await this.addNew()
       }
+      this.$refs.textarea.focus()
     },
     async addNew () {
+      let date = this.date ? new Date(this.date) : new Date()
       let data = {
         ...getNewEntryData(this.text),
-        date: (new Date(this.date)).toISOString(),
+        date: date.toISOString(),
       }
       data._id = data.date
       let e = await this.$store.dispatch('addEntry', data)
       this.$emit('created', e)
-      this.$refs.textarea.value = ''
-      this.date = new Date()
+      this.text = ''
+      this.date = null
     },
     async update () {
       let data = {
