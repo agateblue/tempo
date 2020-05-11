9ecf048ba2c341c80eca94a64463ef710a99f3c2
Agate <me@agate.blue>
2020-05-11 15:20:23 +0200

    Added basic support for setting event data in front matter

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -1,5 +1,14 @@
 import { expect } from 'chai'
-import {parseTags, getNewEntryData, insertTagMarkup, parseQuery, matchTokens} from '@/utils'
+import {
+  parseTags,
+  getNewEntryData,
+  insertTagMarkup,
+  parseQuery,
+  matchTokens,
+  getFrontMatter,
+  parseFrontMatter,
+  quoteFrontMatter
+} from '@/utils'
 
 describe('utils', () => {
   it('can extract tags from text', () => {
@@ -16,6 +25,8 @@ describe('utils', () => {
     const msg = 'Today was quite +happy, but I feel ~tired because of #work.'
     const expected = {
       type: 'entry',
+      event: null,
+      data: null,
       tags: [
         {text: "+happy", id: "happy", type: "feeling", mood: 1, sign: '+'},
         {text: "~tired", id: "tired", type: "feeling", mood: 0, sign: '~'},
@@ -100,5 +111,61 @@ describe('utils', () => {
     }
     const result = matchTokens(entry, tokens)
     expect(result).equal(true)
+  })
+
+  it('get front matter', () => {
+    let entry = `event: id\nfield: value\n---
+    This is the entry text
+    `
+    let expected = 'event: id\nfield: value'
+    const result = getFrontMatter(entry)
+    expect(result).equal(expected)
+  })
+  it('parse front matter', () => {
+    let entry = `event: id\nfield: value`
+    let expected = {event: 'id', field: 'value'}
+    const result = parseFrontMatter(entry)
+    expect(result).deep.equal(expected)
+  })
+  it('get entry data with front matter', () => {
+    const msg = `event: hello\nfoo: bar\namount: 10\n---\nToday was quite +happy, but I feel ~tired because of #work.`
+    const expected = {
+      type: 'entry',
+      tags: [
+        {text: "+happy", id: "happy", type: "feeling", mood: 1, sign: '+'},
+        {text: "~tired", id: "tired", type: "feeling", mood: 0, sign: '~'},
+        {text: "#work", id: "work", type: "tag", mood: null, sign: '#'},
+      ],
+      text: msg,
+      event: 'hello',
+      data: {
+        foo: 'bar',
+        amount: 10,
+      },
+      mood: 1
+    }
+    expect(getNewEntryData(msg)).to.deep.equal(expected)
+  })
+
+  it('get entry data with invalid matter', () => {
+    const msg = `- event: hello\n- foo: bar\n- amount: 10\n---\nToday was quite +happy, but I feel ~tired because of #work.`
+    const expected = {
+      type: 'entry',
+      event: null,
+      data: null,
+      tags: [
+        {text: "+happy", id: "happy", type: "feeling", mood: 1, sign: '+'},
+        {text: "~tired", id: "tired", type: "feeling", mood: 0, sign: '~'},
+        {text: "#work", id: "work", type: "tag", mood: null, sign: '#'},
+      ],
+      text: msg,
+      mood: 1
+    }
+    expect(getNewEntryData(msg)).to.deep.equal(expected)
+  })
+  it('quote front matter', () => {
+    const msg = `event: hello\nfoo: bar\namount: 10\n---\nToday was quite +happy, but I feel ---tired because of #work.`
+    const expected = '```\nevent: hello\nfoo: bar\namount: 10\n```\nToday was quite +happy, but I feel ---tired because of #work.'
+    expect(quoteFrontMatter(msg)).to.deep.equal(expected)
   })
 })
