a66d3175d6af7262218573ba03c2baccc719fd81
Agate <me@agate.blue>
2020-05-31 22:42:46 +0200

    Initial vuetify setup

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,14 @@
 module.exports = {
-  runtimeCompiler: true,
-  chainWebpack: config => {
-    config
-    .plugin('html')
-    .tap(args => {
-      args[0].title = 'Tempo - Find your own rythm'
-      return args
-    })
+  "runtimeCompiler": true,
+  "pwa": {
+    "name": "Tempo",
+    "themeColor": "#422D62",
+    "msTileColor": "#422D62",
+    "workboxOptions": {
+      "cacheId": "v0"
+    }
   },
-  pwa: {
-    name: 'Tempo',
-    themeColor: '#422D62',
-    msTileColor: '#422D62',
-    workboxOptions: {
-      cacheId: process.env['VUE_APP_CACHE_ID'] || 'v0',
-    },
-  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
