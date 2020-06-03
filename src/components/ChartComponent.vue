7b5f8552f8b51a2862eae8a7f4700c1c475b7814
Agate <me@agate.blue>
2020-06-03 13:15:20 +0200

    Now display multiple charts

---
+++ src/components/ChartComponent.vue
@@ -0,0 +1,177 @@
+<template>
+  <v-card class="mb-8">
+
+    <v-card-text v-if="queriedData && queriedData[0]">
+      <table v-if="config.chartType === 'table'">
+        <thead>
+          <tr>
+            <th v-for="field in dataQueryFields" :key="field">{{ field }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, idx) in queriedData" :key="idx">
+            <td v-for="field in dataQueryFields" :key="field">
+              {{ row[field] }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <textarea v-else-if="config.chartType === 'json'" readonly :value="JSON.stringify(queriedData)"></textarea>
+      <chart
+        v-else-if="isFrappeChart"
+        ref="chart"
+        :options="chartOptions">
+      </chart>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        v-if="isFrappeChart"
+        text
+        @click="$refs.chart.chart.export()"
+      >
+        Export chart...
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+        @click="expand = !expand"
+      >
+        Edit chart
+        <v-icon right>{{ expand ? $icons.mdiChevronUp : $icons.mdiChevronDown  }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="expand">
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-textarea
+
+            clearable
+            v-model="dataQuery"
+            ref="textarea"
+            autofocus
+            auto-grow
+            rows="2"
+            label="SQL Query"
+          ></v-textarea>
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+<script>
+import debounce from 'lodash/debounce'
+import alasql from '@/alasql'
+
+export default {
+  props: ['entries', 'config'],
+  components: {
+    Chart:  () => import(/* webpackChunkName: "visualization" */ "@/components/Chart"),
+  },
+  data () {
+    return {
+      expand: false,
+      dataQuery: this.config.query,
+      queriedData: null,
+
+      chartType: this.config.chartType,
+      chartTitle: this.config.label,
+    }
+  },
+  computed: {
+    isFrappeChart () {
+      return ['json', 'table'].indexOf(this.config.chartType) < 0
+    },
+    dataQueryFields () {
+      if (this.queriedData && this.queriedData[0]) {
+        return Object.keys(this.queriedData[0])
+      }
+      return []
+    },
+    chartOptions () {
+      return {
+        data: {
+          datasets: this.getDatasets(this.queriedData),
+          labels: this.getLabels(this.queriedData),
+        },
+        title: this.chartTitle,
+        axisOptions: {
+          xIsSeries: true,
+        },
+        lineOptions: {
+          hideDots: 1,
+        },
+        height: 300,
+        type: this.chartType,
+        maxSlices: 7,
+        colors: ["magenta", "orange"]
+      }
+    },
+  },
+  methods: {
+    async queryData (query) {
+      if (!query) {
+        return null
+      }
+      return await alasql(this.dataQuery,[this.entries]);
+    },
+
+    getLabels (data) {
+      let firstField = Object.keys(data[0])[0]
+      let labels = data.map((r) => {
+        return r[firstField]
+      })
+      labels.reverse()
+      return labels
+    },
+
+    getDatasets (data) {
+      let allFields = Object.keys(data[0]).slice(1)
+      let datasets = []
+      allFields.forEach(f => {
+        let ds = {
+          values: data.map(r => { return r[f] }),
+          name: f,
+        }
+        ds.values.reverse()
+        datasets.push(ds)
+      })
+      return datasets
+    },
+  },
+  watch: {
+
+    dataQuery: {
+      handler: debounce(
+        async function (v) {
+          try {
+            this.queriedData = await this.queryData(v)
+          } catch (e) {
+            console.log('SQL Error', e)
+            this.queriedData = null
+          }
+
+        },
+        500
+      )
+    },
+
+    entries: {
+      deep: true,
+      immediate: true,
+      handler: async function () {
+        try {
+          this.queriedData = await this.queryData(this.dataQuery)
+        } catch (e) {
+          console.log('SQL Error', e)
+          this.queriedData = null
+        }
+
+      }
+    },
+  }
+}
+</script>
--- src/components/Dataviz.vue
+++ src/components/Dataviz.vue
@@ -1,87 +1,32 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
-        <form>
-          <div class="row">
-            <div class="column">
-              <label for="save-queries">Query</label>
-              <select name="saved-queries" id="saved-queries" @input="updateQuery">
-                <option :value="idx" v-for="(dq, idx) in defaultDataQueries" :key="idx">{{ dq.label }}</option>
-              </select>
-            </div>
-            <div class="column">
-              <label for="chart-type">Output</label>
-              <select id="chart-type" name="chart-type" v-model="chartType">
-                <option value="line">Plot line</option>
-                <option value="pie">Pie chart</option>
-                <option value="percentage">Percentage bar</option>
-                <option value="table">Table</option>
-                <option value="json">JSON</option>
-              </select>
-            </div>
-          </div>
-          <hr>
-          <template v-if="queriedData && queriedData[0]">
-            <table v-if="chartType === 'table'">
-              <thead>
-                <tr>
-                  <th v-for="field in dataQueryFields" :key="field">{{ field }}</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(row, idx) in queriedData" :key="idx">
-                  <td v-for="field in dataQueryFields" :key="field">
-                    {{ row[field] }}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <textarea v-else-if="chartType === 'json'" readonly :value="JSON.stringify(queriedData)"></textarea>
-            <chart
-              v-else
-              :options="chartOptions">
-            </chart>
-          </template>
-          <label for="query">Raw query</label>
-          <textarea id="query" name="query" v-model="dataQuery" rows="2"></textarea>
-        </form>
+      <v-col col="12">
+        <chart-component
+          v-for="chart in charts"
+          :key="chart.label"
+          :config="chart"
+          :entries="queryableEntries"></chart-component>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-import debounce from 'lodash/debounce'
-import alasql from '@/alasql'
 import { getCompleteEntry, } from '@/utils'
 
 export default {
   props: ['entries'],
   components: {
-    Chart:  () => import(/* webpackChunkName: "visualization" */ "@/components/Chart"),
+    ChartComponent:  () => import(/* webpackChunkName: "visualization" */ "@/components/ChartComponent"),
   },
   data () {
     return {
       dataQuery: null,
       queriedData: null,
-
-      chartType: "line",
-      chartTitle: "",
     }
-  },
-  created () {
-
-    this.dataQuery = this.defaultDataQueries[0].query
-
-    let help = ''
-    if (this.defaultDataQueries[0].help) {
-      help = ` · ${this.defaultDataQueries[0].help}`
-    }
-    this.chartTitle = `${this.defaultDataQueries[0].label}${help}`
-    this.chartType = this.defaultDataQueries[0].chartType
   },
   computed: {
-    defaultDataQueries () {
+    charts () {
       let defaultDays = 60
       return [
         {
@@ -97,9 +42,8 @@ export default {
           chartType: 'line',
         },
         {
-          help: "Lower is stabler",
           label: "Entries per week",
-          query: `SELECT week, count(*) as entries FROM ? GROUP BY week ORDER BY week DESC LIMIT 16`,
+          query: `SELECT week, count(*) as entries FROM ? GROUP BY week ORDER BY week DESC LIMIT ${Math.floor(defaultDays / 7)}`,
           chartType: 'table',
         },
         {
@@ -107,11 +51,11 @@ export default {
           query: `SELECT date, avg(length(text)) as chars FROM ? GROUP BY date ORDER BY date DESC LIMIT ${defaultDays}`,
           chartType: 'line',
         },
-        {
-          label: "Mood for 'work' tag",
-          query: `SELECT mood, sum(mood) as chars FROM ? WHERE tags->work->present = true GROUP BY mood`,
-          chartType: 'percentage',
-        },
+        // {
+        //   label: "Mood for 'work' tag",
+        //   query: `SELECT mood, sum(mood) as chars FROM ? WHERE tags->work->present = true GROUP BY mood`,
+        //   chartType: 'percentage',
+        // },
         // {
         //   label: "Sleep quality",
         //   query: `SELECT date, sum(tags->sleep->mood) as sleep FROM ? WHERE tags->sleep->present GROUP BY date ORDER BY date DESC LIMIT ${defaultDays}`,
@@ -132,102 +76,6 @@ export default {
         data.push(entry)
       })
       return data
-    },
-    dataQueryFields () {
-      if (this.queriedData && this.queriedData[0]) {
-        return Object.keys(this.queriedData[0])
-      }
-      return []
-    },
-    chartOptions () {
-      return {
-        data: {
-          datasets: this.getDatasets(this.queriedData),
-          labels: this.getLabels(this.queriedData),
-        },
-        title: this.chartTitle,
-        axisOptions: {
-          xIsSeries: true,
-        },
-        lineOptions: {
-          hideDots: 1,
-        },
-        height: 300,
-        type: this.chartType,
-        maxSlices: 7,
-        colors: ["magenta", "orange"]
-      }
-    },
-  },
-  methods: {
-    async queryData (query) {
-      if (!query) {
-        return null
-      }
-      return await alasql(this.dataQuery,[this.queryableEntries]);
-    },
-
-    getLabels (data) {
-      let firstField = Object.keys(data[0])[0]
-      let labels = data.map((r) => {
-        return r[firstField]
-      })
-      labels.reverse()
-      return labels
-    },
-
-    getDatasets (data) {
-      let allFields = Object.keys(data[0]).slice(1)
-      let datasets = []
-      allFields.forEach(f => {
-        let ds = {
-          values: data.map(r => { return r[f] }),
-          name: f,
-        }
-        ds.values.reverse()
-        datasets.push(ds)
-      })
-      return datasets
-    },
-    updateQuery (event) {
-      let conf = this.defaultDataQueries[event.target.value]
-      this.dataQuery = conf.query
-      this.chartType = conf.chartType
-      let help = ''
-      if (conf.help) {
-        help = ` · ${conf.help}`
-      }
-      this.chartTitle = `${conf.label}${help}`
-    },
-  },
-  watch: {
-
-    dataQuery: {
-      handler: debounce(
-        async function (v) {
-          try {
-            this.queriedData = await this.queryData(v)
-          } catch (e) {
-            console.log('SQL Error', e)
-            this.queriedData = null
-          }
-
-        },
-        500
-      )
-    },
-
-    entries: {
-      deep: true,
-      handler: async function () {
-        try {
-          this.queriedData = await this.queryData(this.dataQuery)
-        } catch (e) {
-          console.log('SQL Error', e)
-          this.queriedData = null
-        }
-
-      }
     },
   }
 }
