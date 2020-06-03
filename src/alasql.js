d55819ef27d5877ccda176c4e22466f72aa17ef2
Agate <me@agate.blue>
2020-06-03 10:19:29 +0200

    Basic dataviz

--- src/alasql.js
+++ src/alasql.js
@@ -1,11 +1,4 @@
-var _alasql = null
-export async function getAlasql () {
-  if (!_alasql) {
-    _alasql = (await import(/* webpackChunkName: "alasql" */ "alasql")).default
-  }
-  return alasql
-}
 export default async function alasql (query, data) {
-  let a = await getAlasql()
+  let a = (await import(/* webpackChunkName: "alasql" */ "alasql")).default
   return await a(query, data)
 }
