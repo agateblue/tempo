61bfe5b10c064d5cd3cb0c933258647d2d44b311
Agate <me@agate.blue>
2020-06-04 04:31:25 +0200

    Added date info in timeline

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-    <v-timeline  dense>
-      <v-slide-x-transition
-        group
-      >
+    <v-timeline dense>
+      <template v-for="row in timelineRows">
         <v-timeline-item
-          v-for="row in timelineRows"
+          v-if="row.type === 'entry'"
           :key="row.id"
           left
+          fill-dot
           :color="row.color"
-          class="pb-4"
           small
+          class="pb-4"
         >
-          <entry v-if="row.type === 'entry'" @updated="$emit('updated', $event)" @delete="$emit('delete', $event)" :row="row"></entry>
+          <entry @updated="$emit('updated', $event)" @delete="$emit('delete', $event)" :row="row"></entry>
         </v-timeline-item>
-      </v-slide-x-transition>
+        <v-timeline-item
+          v-else
+          :key="row.repr"
+          hide-dot
+          class="py-8"
+        >
+          <date-entry :row="row"></date-entry>
+        </v-timeline-item>
+      </template>
     </v-timeline>
   </div>
 </template>
@@ -22,6 +29,7 @@
 import MarkdownIt from 'markdown-it'
 import {getCompleteEntry, insertTagMarkup, quoteFrontMatter, getPrettyTimeFromDate} from '@/utils'
 import Entry from './Entry'
+import DateEntry from './DateEntry'
 
 const RENDERER = new MarkdownIt({
   html: true,
@@ -33,20 +41,20 @@ RENDERER.use(require('@toycode/markdown-it-class'), { blockquote: 'blockquote' }
 let aggregateConfigs = [
   {
     id: 'day',
-    repr: (e) => e.date,
+    repr: (e) => e.fullDate.toLocaleDateString(),
   },
-  {
-    id: 'week',
-    repr: (e) => `Week ${e.weeknumber}, ${e.year}`,
-  },
-  {
-    id: 'month',
-    repr: (e) => {
-      let d = new Date(1, e.month, 1)
-      let monthName = d.toLocaleString('default', { month: 'long' })
-      return `${monthName} ${e.year}`
-    }
-  },
+  // {
+  //   id: 'week',
+  //   repr: (e) => `Week ${e.weeknumber}, ${e.year}`,
+  // },
+  // {
+  //   id: 'month',
+  //   repr: (e) => {
+  //     let d = new Date(1, e.month, 1)
+  //     let monthName = d.toLocaleString('default', { month: 'long' })
+  //     return `${monthName} ${e.year}`
+  //   }
+  // },
 ]
 function recordAggregate(config, aggregates, entry, init) {
   let complete = []
@@ -60,16 +68,32 @@ function recordAggregate(config, aggregates, entry, init) {
       if (c.repr(entry) === lastGroup.repr) {
         lastGroup.entries.push(entry)
       } else {
-        complete.push({type: c.id, data: lastGroup})
-        aggregates[c.id].push([{repr: c.repr(entry), entries: [entry]}])
+        complete.push({type: c.id, ...lastGroup})
+        aggregates[c.id].push({repr: c.repr(entry), entries: [entry]})
       }
     })
   }
   return complete
 }
+function insertAggregates(config, rows, aggregates) {
+  let final = []
+  let handled = []
+  rows.forEach((r) => {
+    config.forEach((c) => {
+      aggregates[c.id].forEach((a) => {
+        if (handled.indexOf(a) === -1 && c.repr(r.entry) === a.repr) {
+          handled.push(a)
+          final.push(a)
+        }
+      })
+    })
+    final.push(r)
+  })
+  return final
+}
 export default {
   props: ['entries'],
-  components: {Entry} ,
+  components: {Entry, DateEntry},
   computed: {
     timelineRows () {
       let aggregates = {}
@@ -93,9 +117,11 @@ export default {
         } else if (entry.mood < 0) {
           row.color = "deep-orange" + baseColor + Math.min(Math.abs(entry.mood), 4)
         }
-        let additionalRows = recordAggregate(aggregateConfigs, aggregates, entry, rows.length === 0)
-        rows = [...rows, row, ...additionalRows]
+        recordAggregate(aggregateConfigs, aggregates, entry, rows.length === 0)
+        rows.push(row)
       })
+      rows = insertAggregates(aggregateConfigs, rows, aggregates)
+      console.log(rows)
       return rows
     }
   }
