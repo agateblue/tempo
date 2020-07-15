2492454692eee9a4234460893c9c731d1de2ba19
Agate <me@agate.blue>
2020-07-15 16:10:41 +0200

    Small refactorization of the code

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <v-card tag="section" v-if="!isConfigured" class="mb-8" :color="$theme.card.color">
-      <v-card-title class="headline">Create your board</v-card-title>
+    <v-card
+      tag="section"
+      v-if="!isConfigured || isEditing"
+      class="mb-8"
+      :color="$theme.card.color">
+      <v-card-title class="headline" v-if="isEditing">Update your board</v-card-title>
+      <v-card-title class="headline" v-else>Create your board</v-card-title>
 
       <v-card-text :class="$theme.card.textSize">
-        <board-form></board-form>
+        <board-form @updated="isEditing = false"></board-form>
+
       </v-card-text>
     </v-card>
     <template v-else>
-      <h1 class="mb-4">Your board</h1>
+      <v-row>
+        <v-col>
+          <h1>Your board</h1>
+        </v-col>
+        <v-col>
+          <v-btn class="float-right" @click="isEditing = true" color="secondary">Edit...</v-btn>
+        </v-col>
+      </v-row>
       <div class="board">
-        <v-card class="task-list mr-4" v-for="(list, idx) in $store.getters['taskLists']" :key="idx">
+        <v-card class="task-list mr-4" v-for="(list, idx) in $store.getters['boardLists']" :key="idx">
           <v-card-title class="headline">{{ list.label }}</v-card-title>
         </v-card>
       </div>
@@ -24,9 +37,14 @@ export default {
   components: {
     BoardForm:  () => import(/* webpackChunkName: "tasks" */ "@/components/BoardForm"),
   },
+  data () {
+    return {
+      isEditing: false,
+    }
+  },
   computed: {
     isConfigured () {
-      return this.$store.state.taskLists && this.$store.state.taskLists.length > 0  
+      return this.$store.state.boardConfig && this.$store.state.boardConfig.lists.length > 0  
     }
   }
 }
