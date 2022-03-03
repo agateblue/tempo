a1c24c2f1e034f93f526841d635def9a61d3ba67
agateblue <me+github@agate.blue>
2022-03-03 00:48:49 +0000

    Deploying to gh-pages from @ agateblue/tempo@13f7020bcec900da9e4bcf53f96074f63f092aec 🚀

---
+++ precache-manifest.48fea86d930503557e577ead8739b797.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660f2e51166d43df152b",
+    "url": "/css/app~748942c6.20b0e9a3.css"
+  },
+  {
+    "revision": "d4649a22d18dbfe02d2c",
+    "url": "/css/chunk-vendors~fdc6512a.641c7f32.css"
+  },
+  {
+    "revision": "6c87755abdbae9e080cc",
+    "url": "/css/tasks~31ecd969.726b3234.css"
+  },
+  {
+    "revision": "4fd64079f48ffb20502f7b65add0b61e",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "590cb89a221ce2593ed142ee087bbaca",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "72ce56fb49907bd6a40adcf5d8476490",
+    "url": "/img/icons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "d7b428fa4448c113a842eaeddaca59e5",
+    "url": "/img/icons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "b3d70d173253f0c27919e3380e56617d",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "10b19461880b14a13e2973295ce17c23",
+    "url": "/img/icons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "dfde450660719687325e7438d729c242",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "19ed62be9e7f8165a484d8e300e461f3",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "a007b74ea6ecb8ad981f70fd86799777",
+    "url": "/img/icons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "0025e3dfb6f33ac3808fe25f81e7754c",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "51aa1d8bb7e31f3329ee77d14f4f86b2",
+    "url": "/img/icons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "ad86b2d64314574918e0ca905290de78",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "19ed62be9e7f8165a484d8e300e461f3",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "c31520645451949205c2be4bc6127ebb",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "649c9597a5ffd814d82633d42d85ed08",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "a4f8cab216659a1c5a0220be56c6f40e",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "289a02fd827229b7a152d8b400b32cbd",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "c356beb7bfee10ff0784c0197dc03916",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "400eeb30936b7f4ee831005ab866b363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0320d7d9586b0f1f17e1",
+    "url": "/js/alasql~2a42e354.7d46ea10.js"
+  },
+  {
+    "revision": "660f2e51166d43df152b",
+    "url": "/js/app~748942c6.a4421d49.js"
+  },
+  {
+    "revision": "122c05360f4a8f8c71a8",
+    "url": "/js/chunk-vendors~253ae210.97e3daea.js"
+  },
+  {
+    "revision": "ed9c8d842382c357d9a3",
+    "url": "/js/chunk-vendors~2ebc07f6.ea82b2f0.js"
+  },
+  {
+    "revision": "b67320d304f964d43288",
+    "url": "/js/chunk-vendors~4a7e9e0b.b5d02dd7.js"
+  },
+  {
+    "revision": "e365043227c8a65339b2",
+    "url": "/js/chunk-vendors~7d359b94.7899ad89.js"
+  },
+  {
+    "revision": "3a476f85c05fafb019c9",
+    "url": "/js/chunk-vendors~9c5b28f6.016a9e30.js"
+  },
+  {
+    "revision": "665a69c25595ee3b2605",
+    "url": "/js/chunk-vendors~ec8c427e.06c03e95.js"
+  },
+  {
+    "revision": "d4649a22d18dbfe02d2c",
+    "url": "/js/chunk-vendors~fdc6512a.4a4a6522.js"
+  },
+  {
+    "revision": "6c87755abdbae9e080cc",
+    "url": "/js/tasks~31ecd969.dbf66fe3.js"
+  },
+  {
+    "revision": "21491671ce1d254ed856",
+    "url": "/js/visualization~31ecd969.52059d90.js"
+  },
+  {
+    "revision": "d311f2c71ce2a14481a676bb9f7ae619",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,49 @@
+importScripts("/precache-manifest.48fea86d930503557e577ead8739b797.js", "/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+workbox.loadModule('workbox-routing');
+workbox.loadModule('workbox-strategies');
+
+
+workbox.core.setCacheNameDetails({prefix: "v0"});
+
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+const router = new workbox.routing.Router();
+function matcher ({url}) {
+  let allowed = [
+    `${self.location.protocol}//${self.location.hostname}/`,
+    `${self.location.protocol}//${self.location.hostname}:${self.location.port}/`,
+  ]
+  return allowed.filter((p) => { url.href.startsWith(p)}).length > 0
+}
+var route = new workbox.routing.Route(
+  matcher,
+  new workbox.strategies.NetworkFirst({
+    cacheName: "tempo-cache"
+  })
+)
+router.registerRoute(route)
+router.addCacheListener()
+router.addFetchListener()
+
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+
+// workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
