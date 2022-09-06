b1bcfec589388c6aabe5a30ea8eab509ecb11042
Agate <me@agate.blue>
2022-09-06 22:15:32 +0200

    Display structured data in entries

---
+++ src/components/StructuredData.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <tbody>
+        <tr v-for="row in rows" :key="row.id">
+          <td>
+            {{ row.label }}
+          </td>
+          <td>
+            {{ row.value }}
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+<script>
+export default {
+  props: ['data', 'fields'],
+  computed: {
+    rows () {
+      let r = []
+      for (const key in this.data) {
+        if (Object.hasOwnProperty.call(this.data, key)) {
+          const value = this.data[key];
+          let field = this.fields[key] || {}
+          r.push({
+            id: key,
+            label: field.label || key,
+            value: value
+          })
+        }
+      }
+      return r
+    }
+  }
+}
+</script>
