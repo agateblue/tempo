3808ec0d7ba2553686eecb1bcb3ccb74018a063e
Agate <me@agate.blue>
2023-01-14 19:29:50 +0100

    Added schema validation to YAML editor

--- src/components/YamlEditor.vue
+++ src/components/YamlEditor.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="yaml-editor">
-    {{ error }}
     <div ref="editor" style="height: 500px; width: 100%;"></div>
+    <v-alert
+      color="red darken-2"
+      outlined
+      v-if="errors && errors.length > 0"
+    > 
+      <ul
+        v-for="(e, i) in errors"
+        :key="i"
+      >
+        <li>{{ e }}</li>
+      </ul>
+    </v-alert>
   </div>
-
-</template>
+</template> 
 <script>
 // import isEqual from 'lodash/isEqual'
 import loader from "@monaco-editor/loader";
 import {jsonToYaml, yamlToJson} from '@/utils'
+import { Validator } from '@cfworker/json-schema';
 
 export default {
   props: {
     value: {default: () => {return {foo: 'bar'}}},
-    validator: {}
+    schema: {}
   },
   data () {
     return {
       editor: null,
-      error: null,
+      errors: [],
     }
   },
   mounted () {
     let self = this
+    const validator = new Validator(this.schema);
     loader.init().then((monaco) => {
       const editorOptions = {
         language: "yaml",
@@ -32,7 +44,25 @@ export default {
       };
       self.editor = monaco.editor.create(self.$refs.editor, editorOptions);
       self.editor.getModel().onDidChangeContent(() => {
-        self.$emit('input', yamlToJson(self.editor.getValue()))
+        let data
+        self.errors = []
+        self.$emit('error', self.errors)
+        try {
+          data = yamlToJson(self.editor.getValue())
+        } catch (e) {
+          self.errors = [e]
+          self.$emit('error', self.errors)
+          return
+        }
+        let result = validator.validate(data)
+        if (result.valid) {
+          self.$emit('input', data)
+        } else {
+          self.errors = result.errors.map((e) => {
+            return `${e.keywordLocation}: ${e.error}`
+          })
+          self.$emit('error', self.errors)
+        }
       });
     });
     // this.editor = new JSONEditor({
