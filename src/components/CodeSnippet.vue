74380f56f90a745ffff344f0658901506353d744
Agate <me@agate.blue>
2023-01-15 22:49:02 +0100

    Fix #37: Blueprint documentation

---
+++ src/components/CodeSnippet.vue
@@ -0,0 +1,16 @@
+<template>
+  <pre><code class="code-snippet" ref="code" :class="`language-${language}`">{{ code }}</code></pre>
+</template>
+<script>
+import 'highlight.js/styles/night-owl.css'
+import hljs from 'highlight.js/lib/core';
+import yaml from 'highlight.js/lib/languages/yaml';
+hljs.registerLanguage('yaml', yaml);
+
+export default {
+  props: ['language', 'code'],
+  mounted () {
+    hljs.highlightElement(this.$refs.code);
+  }
+}
+</script>
